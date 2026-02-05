3a35e706ca4138269d793280d29d2b3c8304ce1d
dekimiq <dekimiq@gmail.com>
2026-02-05 22:25:56 +0300

    FEAT: added react-lens smooth scroll

---
+++ src/providers/SmoothScroll.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { ReactNode } from 'react'
+
+import { ReactLenis } from '@studio-freight/react-lenis'
+
+export const SmoothScroll = ({ children }: { children: ReactNode }) => {
+  return (
+    <ReactLenis
+      root
+      options={{
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+        orientation: 'vertical',
+        gestureOrientation: 'vertical',
+        smoothWheel: true,
+        wheelMultiplier: 1,
+        touchMultiplier: 2,
+        infinite: false,
+      }}
+    >
+      {children}
+    </ReactLenis>
+  )
+}
