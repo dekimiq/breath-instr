a15da98ba507b7ba9410c02c260b75ec3472dbcb
dekimiq <dekimiq@gmail.com>
2026-02-06 15:54:13 +0300

    FEAT: improved the hero block

--- src/components/common/landing/Hero/Hero.tsx
+++ src/components/common/landing/Hero/Hero.tsx
@@ -2,16 +2,19 @@
 
 import { useRef } from 'react'
 
-import heroBg from '@/assets/images/landing/hero-bg.png'
+import heroBg from '@/assets/images/landing/hero-bg.jpg'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import Image from 'next/image'
 
 import styles from '@/components/common/landing/Hero/Hero.module.scss'
+import ButtonCta from '@/components/ui/landing/ButtonCta/ButtonCta'
 
 const textsPage = {
-  title: 'Дыхание как технология<br /> восстановления и управления',
+  title: 'Дыхание как технология восстановления и управления',
   desc: 'Это единственная функция тела, работающая автоматически, но доступная и для осознанного управления.',
 }
+
+const MotionImage = motion(Image)
 
 export default function Hero() {
   const container = useRef(null)
@@ -23,21 +26,25 @@ export default function Hero() {
   const y = useTransform(scrollYProgress, [0, 1], ['0%', '30%'])
 
   return (
-    <section ref={container} className={styles.hero}>
+    <section ref={container} className={styles.hero} id="hero">
       <div className={styles.content}>
         <h1 dangerouslySetInnerHTML={{ __html: textsPage.title }} />
         <p>{textsPage.desc}</p>
+        <ButtonCta href="#about" className={styles.btnToBottom}>
+          Начать дышать правильно
+        </ButtonCta>
       </div>
 
-      <motion.div style={{ y, scale: 1.1 }} className={styles.parallaxWrapper}>
-        <Image
+      <div className={styles.parallaxWrapper}>
+        <MotionImage
+          style={{ y, scale: 1.1 }}
           src={heroBg}
           alt="Визуализация легких человека"
           priority
           fill
           className={styles.backgroundImage}
         />
-      </motion.div>
+      </div>
     </section>
   )
 }
