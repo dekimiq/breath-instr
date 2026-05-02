63908f9cffd058321e72c11641ba0b57f91b4adb
dekimiq <dekimiq@gmail.com>
2026-05-02 21:58:54 +0300

    feat: added scroll to block

--- src/components/ui/ButtonCta/ButtonCta.tsx
+++ src/components/ui/ButtonCta/ButtonCta.tsx
@@ -5,16 +5,23 @@ import { motion } from 'framer-motion'
 import styles from '@/components/ui/ButtonCta/ButtonCta.module.scss'
 
 interface ButtonProps {
-  href: string
+  href?: string
   className?: string
   children: React.ReactNode
+  onClick?: (e: React.MouseEvent<HTMLAnchorElement>) => void
 }
 
-export default function ButtonCta({ href, className, children }: ButtonProps) {
+export default function ButtonCta({
+  href,
+  className,
+  children,
+  onClick,
+}: ButtonProps) {
   return (
     <motion.a
       href={href}
       className={`${styles.btnCtaPrimary} ${className || ''} ${'btn'}`}
+      onClick={onClick}
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
     >
