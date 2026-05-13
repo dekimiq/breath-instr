69329a138bed8c959c38e851b40165b63cbc19ab
dekimiq <dekimiq@gmail.com>
2026-05-14 00:32:16 +0300

    fix: fix buld error

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,24 +1,25 @@
 import { EncryptJWT, jwtDecrypt, JWTPayload } from 'jose'
 import { cookies } from 'next/headers'
 
-if (!process.env.JWT_SECRET) {
-  throw new Error('CRITICAL: JWT_SECRET environment variable is not set!')
+function getSecret(): Uint8Array {
+  const secret = process.env.JWT_SECRET
+  if (!secret)
+    throw new Error('CRITICAL: JWT_SECRET environment variable is not set!')
+  return new Uint8Array(
+    secret.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16))
+  )
 }
-
-const SECRET = new Uint8Array(
-  process.env.JWT_SECRET.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16))
-)
 
 export async function encrypt(payload: JWTPayload) {
   return await new EncryptJWT(payload)
     .setProtectedHeader({ alg: 'dir', enc: 'A256GCM' })
     .setIssuedAt()
     .setExpirationTime('24h')
-    .encrypt(SECRET)
+    .encrypt(getSecret())
 }
 
 export async function decrypt(input: string) {
-  const { payload } = await jwtDecrypt(input, SECRET, {
+  const { payload } = await jwtDecrypt(input, getSecret(), {
     contentEncryptionAlgorithms: ['A256GCM'],
   })
   return payload
